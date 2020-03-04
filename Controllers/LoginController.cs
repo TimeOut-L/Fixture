bfbb7b2f68d1dd0d2cbdd3e9880c9d6cfdba394c
TimeOut-L <46495900+TimeOut-L@users.noreply.github.com>
2020-03-04 18:54:39 +0800

    Login and resource

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FixtureManagement.Controllers
+{
+    public class LoginController :Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoLogin(String code ,String password)
+        {
+            
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
