576ffac366282cddd79eb124cd4eb31caa6bf815
lin <15*****488@qq.com>
2020-05-07 10:14:42 +0800

    添加类别管理的后端代码

--- Models/Context/FixtureManagerContext.cs
+++ Models/Context/FixtureManagerContext.cs
@@ -27,5 +27,6 @@ namespace FixtureManagement.Models.Context
         public DbSet<FixtureEntity> FixtureEntities { get; set; }
         public DbSet<FixtureInRecord> InRecords { get; set; }
         public DbSet<FixtureOutRecord> OutRecords { get; set; }
+        public DbSet<FixtureFamily> Family { get; set; }
     }
 }
