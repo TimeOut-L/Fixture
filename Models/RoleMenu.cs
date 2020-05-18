b9edee4ec98210f2b7f7374e9fd01c372372c888
TimeOut-L <46495900+TimeOut-L@users.noreply.github.com>
2020-05-18 14:39:41 +0800

    整合ManagerFamily 更新数据库

--- Models/RoleMenu.cs
+++ Models/RoleMenu.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +10,12 @@ namespace FixtureManagement.Models
     public class RoleMenu
     {
         public int ID { get; set; }
+        [Key]
+        [Column(Order = 1)]
+
         public int RoleID { get; set; }
+        [Key]
+        [Column(Order =2)]
         public string MenuID { get; set; }
     }
 }
