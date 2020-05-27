e0cdb9c51a32ab252d86c5f51b5464341a47cae6
MrBen2019 <2418784098@qq.com>
2020-05-27 23:00:01 +0800

    完善报修申请流程功能

--- Controllers/FixtureRepairController.cs
+++ Controllers/FixtureRepairController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using FixtureManagement.Common;
+using FixtureManagement.filter;
+using FixtureManagement.Models;
+using FixtureManagement.Models.Context;
+using FixtureManagement.Service;
+using FixtureManagement.Service.impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +12,141 @@ using System.Web.Mvc;
 
 namespace FixtureManagement.Controllers
 {
+    [LoginCheckFilter]
+    [UserFilter]
     public class FixtureRepairController : Controller
     {
-        // GET: FixtureRepair
-        public ActionResult Index()
+        FixtureManagerContext context = new FixtureManagerContext();
+        public UserService userService { get; set; }
+        RepairService repairService = new RepairServiceImpl();
+        // GET: FixtureRepair/Apply
+        public ActionResult Apply()
         {
             return View();
         }
+
+        // GET: FixtureRepair/Dealwith
+        public ActionResult DealWith()
+        {
+            return View();
+
+        }
+
+
+        //添加报修申请
+        [HttpPost]
+        public ActionResult AddRepairRecord()
+        {
+            FixtureRepair fr = new FixtureRepair();
+            var user = (CurrentUserWorkCell)Session["CurrentUser"];
+            fr.RepBy = user.code;//获取当前申请人的工号
+            fr.RepByName = userService.GetUserByCode(user.code).Name;//获取当前申请人姓名
+            fr.Code = Request["code"];
+            fr.SeqID = Convert.ToInt32(Request["seqID"]);
+            fr.faultDes = Request["faultDes"];
+            fr.faultPic = Request["faultPic"];
+            fr.DealBy = "";
+            fr.DealByName = "";
+            fr.DealRes = "";
+            fr.State = "受理中";
+            context.repairs.Add(fr);
+            context.SaveChanges();
+            var data = new
+            {
+                success = true,
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+
+        }
+
+        //查询本用户能查看的报废申请
+        [HttpPost]
+        public ActionResult ReadRepairWithUser()
+        {
+            var user = (CurrentUserWorkCell)Session["CurrentUser"];
+            string currentcode = user.code;
+            List<FixtureRepair> fixtureRepairs = context.repairs.SqlQuery("select * from FixtureRepair where RepBy=" + currentcode).ToList();
+            return Json(fixtureRepairs, JsonRequestBehavior.AllowGet);
+
+        }
+        //查询本车间正在初审的报废申请
+        [HttpPost]
+        public ActionResult ReadRepairState()
+        {
+            var user = (CurrentUserWorkCell)Session["CurrentUser"];
+            string State = "受理中";
+            string currentWorkCell = user.workCell;
+            List<FixtureRepair> fixturePurchases = context.repairs.SqlQuery("select * from FixtureRepair fr join UserRole ur on fr.RepBy=ur.UserCode where (State='" + State + "' or State='通过') and WorkCell='" + currentWorkCell + "'").ToList();
+            return Json(fixturePurchases, JsonRequestBehavior.AllowGet);
+        }
+
+        //删除报修信息
+        [HttpPost]
+        public ActionResult DeleteRepairRecord()
+        {
+            string JSONData = Request["record"];
+            string[] arr = JSONData.Split(';');
+            if (!repairService.Delete(arr[0],Convert.ToInt32(arr[1])))
+            {
+                var exception = new
+                {
+                    success = false,
+                    msg = "可能有些记录不存在"
+                };
+                return Json(exception, JsonRequestBehavior.AllowGet);
+            }
+            var data = new
+            {
+                success = true,
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        //修改报废申请状态
+        [HttpPost]
+        public ActionResult UpdateRepair()
+        {
+            string JSONData = Request["record"];
+            string[] arr = JSONData.Split(';');
+            var _record = repairService.FindByCode_SeqId(arr[0], Convert.ToInt32(arr[1]));
+            if (_record == null)
+            {
+                var error = new
+                {
+                    succes = false,
+                    msg = "该条记录不存在请刷新表格"
+                };
+                return Json(error, JsonRequestBehavior.AllowGet);
+            }         
+            _record.RepBy = _record.RepBy;
+            _record.RepByName = _record.RepByName;
+            _record.Code = _record.Code;
+            _record.SeqID = _record.SeqID;
+            _record.faultDes = _record.faultDes;
+            _record.faultPic = _record.faultPic;
+            var user = (CurrentUserWorkCell)Session["CurrentUser"];
+            _record.DealBy = user.code;
+            _record.DealByName = userService.GetUserByCode(user.code).Name;//获取当前申请人姓名
+            _record.DealRes = "";
+            _record.State = arr[2];
+            if (!repairService.Update(_record))
+            {
+                var error = new
+                {
+                    succes = false,
+                    msg = "编辑保存失败"
+                };
+                return Json(error, JsonRequestBehavior.AllowGet);
+            }
+
+            var data = new
+            {
+                success = true,
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+
+
     }
 }
---
+++ Service/impl/RepairServiceImpl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FixtureManagement.Models;
+using FixtureManagement.Models.Context;
+
+namespace FixtureManagement.Service.impl
+{
+    public class RepairServiceImpl : RepairService
+    {
+        private FixtureManagerContext context = FixtureDbContextHelper.GetCurrentContext();
+
+        public bool Delete(string Code, int SeqId)
+        {
+            var _record = context.repairs.Find(Code, SeqId);
+            if (_record != null)
+            {
+                context.repairs.Remove(_record);
+                context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public FixtureRepair FindByCode_SeqId(string Code, int SeqId)
+        {
+            var _record = context.repairs.Find(Code, SeqId);
+            if (_record == null)
+            {
+                return null;
+            }
+            return _record;
+        }
+
+        public bool Update(FixtureRepair fr)
+        {
+            var _record = context.repairs.AsNoTracking().FirstOrDefault(o => (o.Code == fr.Code && o.SeqID == fr.SeqID));
+            if (_record == null)
+            {
+                return false;
+            }
+            else
+            {
+                context.repairs.Attach(fr);
+                context.Entry<FixtureRepair>(fr).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
